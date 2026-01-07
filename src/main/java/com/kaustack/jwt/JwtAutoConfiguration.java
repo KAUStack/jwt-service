e08508bd80d257b252075c05b2cedcdf29810a50
Abdulaziz <me@abdaziz.dev>
2026-01-07 13:05:38 +0300

    Introduce JWT auto-configuration for Spring Boot.

---
+++ src/main/java/com/kaustack/jwt/JwtAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.kaustack.jwt;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "com.kaustack.jwt")
+public class JwtAutoConfiguration {
+}
